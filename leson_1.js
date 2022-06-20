5287047228ec8e99b7af2aeffdde3759b39e066a
unknown <elvyudina@gmail.com>
2022-06-21 00:13:30 +0300

    error correction

--- leson_1.js
+++ leson_1.js
@@ -44,7 +44,7 @@ let item_5 = item_3
 //Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
 
 let item_6 = 15
-let item_6_type = typeof item_6
+let item_6_type = item_6
 console.log('item_6 == ' + item_6, typeof item_6)
 console.log('item_6_type == ' + item_6_type, typeof item_6_type)
 
@@ -56,7 +56,7 @@ console.log(typeof ('item_6')) //вывели на консоль у убеде�
 //Присвоить переменной item_7_type тип переменной item_7
 // Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——
 
-let item_7_type = typeof item_7
+let item_7_type = item_7
 console.log('item_7 == ' + item_7, typeof item_7)
 console.log('item_7_type == ' + item_7_type, typeof item_7_type)
 
